b15f56b7baa6ea5f11ae253f8ea035b01b2079ff
Dels1x <delsixreborn@gmail.com>
2022-04-21 17:54:58 +0300

    1. Java tutorial for beginners

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        !EBLAN
-        System.out.println("sosi hui i ne psihui, java ne tyurma sidi kaifui");
+
+        System.out.println("\t\"GG\"\n");
+        System.out.println("\t\\tEZ\\n\n");
+
+        // This is my first comment I wrote with JAVA хд (stint is cool, love GMD | Общение)
+
+        /*This
+        is
+        stupidly
+        long
+        comment
+        lol
+         */
+
+
     }
 }
