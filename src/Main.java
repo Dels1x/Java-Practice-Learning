4abc6ca493d05a9fa5cb8c4ca14787ebe495971d
Dels1x <delsixreborn@gmail.com>
2022-04-21 19:02:39 +0300

    3. swap two variables

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,29 @@ public class Main {
         lol
          */
 
+        int x = 32;
+        float y = 6.42f;
+        boolean rx = true;
+        char symbol = '6';
+        String string = String.format("My number is %f = %b", y, rx);
+
+        System.out.println(string);
+
+        String strOne = "String one";
+        String strTwo = "String two";
+        String temp;
+
+        System.out.println(strOne+" "+strTwo);
+
+        temp = strTwo;
+        strTwo = strOne;
+        strOne = temp;
+
+        System.out.println(strOne+" "+strTwo);
+
+        // Bro Code you are genius. I've never seen such a cool example LOL
+        // I'm so happy that I didn't waste 12$ at these courses on udemy
+
 
     }
 }
