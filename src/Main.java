b08e06920784205a436c0060617ef1d91d5e99c7
Dels1x <delsixreborn@gmail.com>
2022-05-03 18:12:04 +0300

    8. random numbers

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,17 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        Random random = new Random();
 
-        double x = 3.14;
-        byte y = -10;
-        int z = 6;
+        int x = random.nextInt(1, 10);
+        double y = random.nextDouble(1, 10);
+        boolean z = random.nextBoolean();
 
-        double min = Math.min(x, y);
-        double max = Math.max(x, y);
-        double y_abs = Math.abs(y);
-        double sqrt = Math.sqrt(z);
-        long round = Math.round(x);
-        long ceil = (long) Math.ceil(x);
-        long floor = (long) Math.floor(x);
-
-
-        System.out.printf("min: %f max: %f abs of y: %f koren' %f round %2d ceil %2d floor %2d", min, max, y_abs, sqrt, round, ceil, floor);
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter side x:");
-        double a = scanner.nextDouble();
-        System.out.println("Enter side y:");
-        double b = scanner.nextDouble();
-
-        double c = Math.sqrt((x*x)+(y*y));
-
-        System.out.println("The hypotenuse is "+c);
-
-        // kirill lox
+        System.out.println(x + y);
+        System.out.println(z);
 
     }
 }
