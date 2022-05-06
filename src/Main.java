283ad378b498ba34a1b3fb0ec0f072a31d1c5904
Dels1x <delsixreborn@gmail.com>
2022-05-06 16:47:17 +0300

    10. switches

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String day = "Friday";
 
-        int age = scanner.nextInt();
-
-        if (age >= 75) {
-            System.out.println("C kakih por na kladbishe poyavilsya internet?");
-        }
-        else if(age >= 18) {
-            System.out.println("Ebat' ti stariy!");
-        }
-        else {
-            System.out.println("Ebat' ti molodoy!");
+        switch(day) {
+            case "Sunday": System.out.println("This is sunday!");
+            break;
+            case "Monday": System.out.println("HLTV top 20 today in 20:00!");
+            break;
+            case "Friday": System.out.println("Weekend tomorrow!");
+            break;
+            case "Tuesday": System.out.println("This is tuesday!");
+            break;
+            case "Wednesday": System.out.println("This is wednesday!");
+            break;
+            case "Saturday": System.out.println("This is saturday!");
+            break;
+            case "Thursday": System.out.println("This is thursday!");
+            break;
+            default: System.out.println("This is not a day!");
         }
 
 
