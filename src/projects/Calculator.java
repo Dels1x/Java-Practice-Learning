2b28a063096d4626fd2f6e03db9e3e6c2f8dbe7f
Dels1x <delsixreborn@gmail.com>
2022-05-08 23:22:16 +0300

    First JAVA project done: very stupid version of calculator

---
+++ src/projects/Calculator.java
@@ -0,0 +1,27 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1st integer: ");
+        long x = scanner.nextLong();
+        System.out.println("Enter 2nd integer: ");
+        long y = scanner.nextLong();
+        scanner.nextLine();
+        System.out.println("Enter what you want to do with the numbers (+ - * /): ");
+        String z = scanner.nextLine();
+
+        switch (z) {
+            case "+" -> System.out.println(x + y);
+            case "-" -> System.out.println(x - y);
+            case "*" -> System.out.println(x * y);
+            case "/" -> System.out.println(x / y);
+        }
+
+    }
+}
