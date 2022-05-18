21d4958326b1cc8721dcf62e481e4bba11683deb
Dels1x <delsixreborn@gmail.com>
2022-05-18 17:01:41 +0300

    12. while loop

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* logical operators = used to connect two or more expressions
-
-                               && = (AND) both conditions must be true
-                               || = (OR) one of connections must be true
-                               ! = (NOT) reserves boolean value of condition */
-
-        int temp = 26;
-
-        if(temp >= 30) {
-            System.out.println("It is hot outside!");
-        }
-        else if(temp >= 15 && temp < 30) {
-            System.out.println("It is warm outside!");
-        }
-        else{
-            System.out.println("It is cold oustide!");
-        }
+        String name = "";
 
         Scanner scanner  = new Scanner(System.in);
-        System.out.println("Press Q or q to quit");
-        String response = scanner.next(); //
-        if(response.equals("q") || response.equals("Q")) {
-            System.out.println("Bye!");
-        }
-        else{
-            System.out.println("ok dude");
+        while(name.isBlank()) {
+            System.out.println("Enter your name.");
+            name = scanner.nextLine();
         }
 
+        System.out.println("Hello, " + name + "!");
 
     }
 }
