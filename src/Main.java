c56ca3f92b8427e87f0d16b96c23d0e68facfbe2
Dels1x <delsixreborn@gmail.com>
2022-05-25 22:04:21 +0300

    14. nested loops

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i <= 10; i++){
-            System.out.println(i);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows");
+        int rows = scanner.nextInt();
+        System.out.println("How many columns");
+        int columns = scanner.nextInt();
+        System.out.println("Enter symbol");
+        scanner.nextLine();
+        String symbol = scanner.nextLine();
+
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= columns; j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
         }
 
     }
