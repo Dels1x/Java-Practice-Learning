b3281c0bf86bb99e0ad0845324b6d6cf6f08e1b7
Dels1x <delsixreborn@gmail.com>
2022-05-25 23:23:05 +0300

    15. arrays

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many rows");
-        int rows = scanner.nextInt();
-        System.out.println("How many columns");
-        int columns = scanner.nextInt();
-        System.out.println("Enter symbol");
-        scanner.nextLine();
-        String symbol = scanner.nextLine();
+        // array = you already know what array is you fucking retard you learned python dumbass
 
+        String[] cars = {"Hyndai Tiburon", "Audi TT", "Toyota Corolla", "Peugeot 206"};
 
-        for(int i = 1; i <= rows; i++){
-            for(int j = 1; j <= columns; j++){
-                System.out.print(symbol);
-            }
-            System.out.println();
+        for(int i = 0; i < 4; i++){
+            System.out.println(cars[i]);
         }
 
+        cars[2] = "Nissan 370Z";
+
+        for(int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
+        
+        System.out.println(cars);
+
+
+        int[] years = new int[4];
+
+        years[0] = 2004;
+        years[1] = 2007;
+        years[2] = 2004;
+        years[3] = 2003;
+
+        for(int i = 0; i < years.length; i++){
+            System.out.println(years[i]);
+        }
     }
 }
