40a37981f122e8c05f72fa4aec9795f1f158344f
Dels1x <delsixreborn@gmail.com>
2022-05-26 15:03:09 +0300

    17. String methods

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] tictactoe = new int[3][3];
+        /*
+            String = a reference data type that can store on or more characters
+                     reference data types have access to useful methods
+         */
 
-        int index = 0;
+        String name = "delsix";
+        String name2 = "";
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                index++;
-                tictactoe[i][j] = index;
-            }
-        }
+        boolean result1 = name.equals("Delsix");
+        boolean result2 = name.equalsIgnoreCase("Delsix"); // ignores upper/lowercase
+        int result3 = name.length();
+        char result4 = name.charAt(name.length()-1);
+        int result5 = name.indexOf(result4);
+        boolean result6 = name2.isEmpty();
+        String result7 = name.replace("i", "e").replace("d", "s");
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                System.out.print(tictactoe[i][j] + " ");
-            }
-            System.out.println();
-        }
-
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
 
     }
 }
