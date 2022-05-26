a562e5dd0f54be658107bde22757de2a61940e43
Dels1x <delsixreborn@gmail.com>
2022-05-26 16:50:45 +0300

    18. wrapper classes

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*
-            String = a reference data type that can store on or more characters
-                     reference data types have access to useful methods
-         */
+        Boolean a = true;
+        Character b = 's';
+        Integer c = 123;
+        Double d = 12.3344577;
+        String e = "delsix";
 
-        String name = "delsix";
-        String name2 = "";
+        String f = c.toString();
 
-        boolean result1 = name.equals("Delsix");
-        boolean result2 = name.equalsIgnoreCase("Delsix"); // ignores upper/lowercase
-        int result3 = name.length();
-        char result4 = name.charAt(name.length()-1);
-        int result5 = name.indexOf(result4);
-        boolean result6 = name2.isEmpty();
-        String result7 = name.replace("i", "e").replace("d", "s");
-
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
-        System.out.println(result5);
-        System.out.println(result6);
-        System.out.println(result7);
+        System.out.print(f);
 
     }
 }
