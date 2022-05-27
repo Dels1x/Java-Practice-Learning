8e0f03a8bcb00b864d7eaff4589aafde2414923d
Dels1x <delsixreborn@gmail.com>
2022-05-27 13:47:54 +0300

    21. for-each loop

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<String>> Hardware = new ArrayList<ArrayList<String>>();
+       String[] animals = {"cat", "dog", "donkey", "eagle"};
 
-        ArrayList<String> CPUs = new ArrayList<String>();
-        ArrayList<String> GPUs = new ArrayList<String>();
-        ArrayList<String> RAMs = new ArrayList<String>();
-
-        CPUs.add("intel i5 12400f");
-        CPUs.add("AMD Ryzen 5800X");
-        CPUs.add("AMD Phenom B55");
-
-        GPUs.add("AMD RX 6600 XT");
-        GPUs.add("GeForce RTX 3080 Ti");
-        GPUs.add("GeForce GTX 260");
-
-        RAMs.add("16GB");
-        RAMs.add("64GB");
-        RAMs.add("4GB");
-
-        Hardware.add(CPUs);
-        Hardware.add(GPUs);
-        Hardware.add(RAMs);
-
-        for(int i = 0; i < Hardware.size(); i++){
-            for(int j = 0; j < Hardware.size(); j++){
-                System.out.print(Hardware.get(j).get(i) + " ");
-            }
-
-            System.out.println(CPUs.get(i).isBlank());
-        }
+       for(String i : animals){ // basically python's "for i in animals:" in Java
+           System.out.println(i);
+       }
 
     }
 }
