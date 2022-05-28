b465389410e2e311b197b52212aee91e3e18a465
Dels1x <delsixreborn@gmail.com>
2022-05-28 10:02:44 +0300

    26. OOP

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    String make = "Nissan";
+    String model = "370Z";
+    int year = 2004;
+    String color = "green";
+    double price = 15000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final double PI = 3.14159;
+        // object = an instance of a class that may contain attributes and methods
+        // example: (phone, desk, coffee cup, bottle)
 
-        // final keyword basically makes a constanta out of variable, so you can no longer change it.
-        // YOU SHOULD WRITE IT WITH UPPERCASE LETTERS
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
 
-        System.out.println(PI);
+        System.out.println(myCar1.make);
+        System.out.println(myCar1.model);
+        System.out.println(myCar1.year);
+        System.out.println(myCar1.color);
+
+        myCar1.brake();
+
+
+        System.out.println(myCar2.make);
+        System.out.println(myCar2.model);
+        System.out.println(myCar2.year);
+        System.out.println(myCar2.color);
+
+        myCar2.brake();
 
     }
 
