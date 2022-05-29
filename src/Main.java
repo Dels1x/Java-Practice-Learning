d5bb6b0067f8061f4148d32d61cbfe6e9e4dbc00
Dels1x <delsixreborn@gmail.com>
2022-05-29 23:48:50 +0300

    31. array of objects

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x = 27 / 5;
-        System.out.print(x);
+        Food[] refrigerator = new Food[3];
+
+        Food food1 = new Food("Ice cream");
+        Food food2 = new Food("Pizza");
+        Food food3 = new Food("Tiramisu");
+
+        refrigerator[0] = food1;
+        refrigerator[1] = food2;
+        refrigerator[2] = food3;
+
+        for(Object i : refrigerator){
+            System.out.println(i);
+        }
 
     }
 
