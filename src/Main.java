01ad753c03b206cb62933f8b09abe96cefddb676
Dels1x <delsixreborn@gmail.com>
2022-06-02 16:52:30 +0300

    43. dynamic polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
-import package2.C;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // polymorphism = greek word for poly - "many", morph = "form".
-        //                The ability of an object to identify as more than one type
+        // polymorphism = many shapes/forms
+        // dynamic = after compilation (during runtime)
 
+        // ex. A corvette is a: corvette, a car, a vehicle and an object
 
-        Car car = new Car();
-        Bicycle bicycle = new Bicycle();
-        Boat boat = new Boat();
+        Scanner scanner = new Scanner(System.in);
+        Animal animal;  // we declare animal class, as we don't know what user will pick
 
-        Vehicle[] racers = {car, bicycle, boat};
+        System.out.println("What pet do you want?");
+        System.out.print("1=cat, 2=dog: ");
+        int choice =  scanner.nextInt();
 
-        for(Vehicle i : racers)  { // you could also use "object" instead of "Vehicle"
-            i.go();
+        if(choice==1) {
+            animal = new Cat();
         }
+        else {
+            animal = new Dog();
+        }
+
+        animal.speak();
+
 
     }
 
