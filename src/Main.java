326008ad6a1273067dea8ad5b1d38e955c84706e
Dels1x <delsixreborn@gmail.com>
2022-06-02 17:32:35 +0300

    44. exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // polymorphism = many shapes/forms
-        // dynamic = after compilation (during runtime)
+        try {
+            Scanner scanner = new Scanner(System.in);
 
-        // ex. A corvette is a: corvette, a car, a vehicle and an object
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-        Animal animal;  // we declare animal class, as we don't know what user will pick
-
-        System.out.println("What pet do you want?");
-        System.out.print("1=cat, 2=dog: ");
-        int choice =  scanner.nextInt();
-
-        if(choice==1) {
-            animal = new Cat();
+            System.out.println(x / y);
         }
-        else {
-            animal = new Dog();
+        catch(ArithmeticException e) {
+            System.out.println("You can divide by 0 retard");
         }
-
-        animal.speak();
+        catch(InputMismatchException e) {
+            System.out.println("Enter a fricking number god damn it");
+        }
+        catch(Exception e) {
+            System.out.println("laziness");
+        }
 
 
     }
