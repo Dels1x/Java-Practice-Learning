90d06c79b74ec3217cb9f52dc1d85f27779476e1
Dels1x <delsixreborn@gmail.com>
2022-06-02 14:09:38 +0300

    41. interface

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Encapsulation = attributes of the class will be hidden or private,
-        //                 can be accessed only trough methods (getters and setters).
-        //                 you should make attributes private if you don't have a reason to make them public/protected
+        // interface = a template that can be applied to a class.
+        //             similar to inheritance, but specifies what class has/must do
+        //             classes can apply more than one interface, inheritance is limited to 1 super
 
-        Car car1 = new Car("Dodge", "Challenger", 2019);
-        Car car2 = new Car("Nissan", "370Z", 2004);
+    Rabbit rabbit = new Rabbit();
+    Hawk hawk = new Hawk();
+    Fish fish = new Fish();
 
-        System.out.println(car1.getMake());
-        System.out.println(car1.getModel());
-        System.out.println(car1.getYear()+"\n");
-
-        car1.setYear(2022);
-
-        System.out.println(car1.getMake());
-        System.out.println(car1.getModel());
-        System.out.println(car1.getYear());
-
-        car1.setMake("");
-        car1.setModel("");
-        car1.setYear(228);
-
-        System.out.println(car1);
-        System.out.println(car2);
-
-        // don't do car1 = car2 to copy objects. because they will share the same adress in memory. so it's not really
-        // copy. it's more like another name for the same object
-
-        car2.copy(car1); // do this instead
-        Car car3 = new Car(car1); // or this
-
-        System.out.println(car1);
-        System.out.println(car2);
-        System.out.println(car3);
-
-        //xd
+    rabbit.flee();
+    hawk.hunt();
+    fish.flee();
+    fish.hunt();
 
     }
 
