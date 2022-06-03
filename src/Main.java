fdc31549d702f7d9586df40e1e724eca9cadbd28
Dels1x <delsixreborn@gmail.com>
2022-06-03 19:27:58 +0300

    45. FileWriter (write a file)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File("C:\\Users\\38099\\IdeaProjects\\BroCode\\src\\secret_message.txt");
-
-        if(file.exists()) {
-            System.out.println("That file exists 0_0");
-            System.out.println(file);
-            file.delete();
+        try {
+            FileWriter fw = new FileWriter("poem.txt");
+            fw.write("check heaven check heaven cover me");
+            fw.close();
         }
-        else {
-            System.out.println("That file doesn't exist x_x");
+        catch(IOException e){
+            e.printStackTrace();
         }
 
-        // ?
 
     }
 
