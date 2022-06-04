51446ab13a2755956e19faa2c65f94feacffdc72
Dels1x <delsixreborn@gmail.com>
2022-06-04 16:48:08 +0300

    48. audio

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,35 @@
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+import javax.sound.sampled.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
 
-        // FileReader = read the content of a file as a stream of characters. return int byte value //
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            FileReader fr = new FileReader("poem.txt");
-            int data = fr.read();
-            while(data != -1) {
-                System.out.print((char)data);
-                data = fr.read();
+        File file = new File("UP.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        clip.start(); // runs as a background thread. so the script will be terminated, once the code is done
+
+        String response = "";
+
+        while(!response.equals("Q")){
+            System.out.println("R - Restart, P - Play, S - Stop, Q - Quit");
+            response = scanner.next().toUpperCase(Locale.ROOT);
+            switch(response){
+                case("R") -> clip.setMicrosecondPosition(0);
+                case("P") -> clip.start();
+                case("S") -> clip.stop();
+                case("Q") -> System.out.println("Bye :)");
+                default -> System.out.println("Not a valid response");
             }
-            fr.close();
         }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-
 
     }
-
-
 }
