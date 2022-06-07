26deca82626db9fc91ce4721cb043effd85d5860
Dels1x <delsixreborn@gmail.com>
2022-06-07 19:14:29 +0300

    52. buttons

--- src/Main.java
+++ src/Main.java
@@ -7,79 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // JFrame = a GUI component that functions as container to hold other elements
+        // JButton = a button that performs an action when clicked on
 
-        JFrame frame = new JFrame();  // creates a frame
-
-        ImageIcon image = new ImageIcon("Dels1x.png"); // create an image icon
-        ImageIcon image2 = new ImageIcon("1111.png"); // create an image icon
-
-        frame.setIconImage(image.getImage()); // change icon of frame
-        frame.getContentPane().setBackground(new Color(0xEEEEEE));
-        frame.setVisible(true); // makes the frame visible
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // end the script once user closes window
-        frame.setSize(532, 552); // sets resolution of a frame
-        frame.setLayout(null);
-        frame.setResizable(true); // prevents frame from being resized
-        frame.setTitle("delsix's lab"); // sets title of a frame
-        //frame.pack(); // unpacks everything and PC optimizes it to have everything on screen
-
-        JPanel redPanel = new JPanel();
-        JPanel bluePanel = new JPanel();
-        JPanel greenPanel = new JPanel();
-        JPanel yellowPanel = new JPanel();
-        JPanel magentaPanel = new JPanel();
-
-        redPanel.setBackground(Color.red);
-        bluePanel.setBackground(Color.blue);
-        greenPanel.setBackground(Color.green);
-        yellowPanel.setBackground(Color.yellow);
-        magentaPanel.setBackground(Color.magenta);
-
-        redPanel.setBounds(0,  0, 128, 128);
-        bluePanel.setBounds(128,  0, 128, 128);
-        greenPanel.setBounds(0,  128, 256, 128);
-        yellowPanel.setBounds(256,  0, 256, 256);
-        magentaPanel.setBounds(0, 256, 512, 256);
-
-
-        JLabel label1 = new JLabel();
-        JLabel label2 = new JLabel();
-        JLabel label3 = new JLabel();
-
-        label1.setText("Hi kids :)");
-        label2.setText("Do you like violence?");
-
-        greenPanel.setLayout(new BorderLayout());
-        yellowPanel.setLayout(null);
-        magentaPanel.setLayout(new BorderLayout());
-
-        label1.setVerticalAlignment(JLabel.CENTER);
-        label1.setHorizontalAlignment(JLabel.CENTER);
-        label2.setBounds(30, 0, 165, 75);
-        label3.setVerticalAlignment(JLabel.CENTER);
-        label3.setHorizontalAlignment(JLabel.CENTER);
-
-        label3.setText("I only crash these cars"); // set text of label
-        label3.setIcon(image2); // sets an image of label
-        label3.setHorizontalTextPosition(JLabel.CENTER); // sets text LEFT, CENTER, RIGHT of imageIcon
-        label3.setVerticalTextPosition(JLabel.TOP); // sets text TOP, CENTER, BOTTOm of imageIcon
-        label3.setForeground(Color.white); // sets font color of text
-        label3.setFont(new Font("MV Boli", Font.PLAIN, 32)); // sets font, size of text
-        label3.setIconTextGap(-6); // sets gap between image and text
-        label3.setBorder(BorderFactory.createLineBorder(Color.black, 5)); // sets a border for our label
-        label3.setVerticalAlignment(JLabel.CENTER); // sets a vertical position of our label within label
-        label3.setHorizontalAlignment(JLabel.CENTER); // sets a horizontal position of our label within label
-
-        greenPanel.add(label1);
-        yellowPanel.add(label2);
-        magentaPanel.add(label3);
-
-        frame.add(redPanel); // unpacks panel
-        frame.add(bluePanel); // unpacks panel
-        frame.add(greenPanel); // unpacks panel
-        frame.add(yellowPanel); // unpacks panel
-        frame.add(magentaPanel); // unpacks panel
+        new MyFrame();
 
 
     }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,7 +1,75 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button1;
+    JButton button2;
+    JLabel label;
+    int i = 0;
     MyFrame(){
+
+        ImageIcon icon = new ImageIcon("diamond_helmet_64x64.png");
+        button1 = new JButton();
+        button2 = new JButton();
+        button1.setBounds(50, 50, 150, 150);
+        button1.setText("Click me");
+        button1.setHorizontalTextPosition(JLabel.CENTER);
+        button1.setVerticalTextPosition(JLabel.BOTTOM);
+        button1.setFocusable(false);
+        button1.setIcon(icon);
+        button1.setIconTextGap(-15);
+        button1.setForeground(new Color(0xAA00FF));
+        button1.setBorder(BorderFactory.createEtchedBorder());
+        button1.addActionListener(this);
+
+        button2 = new JButton();
+        button2.setBounds(50, 200, 150, 150);
+        button2.setText("Click me");
+        button2.setHorizontalTextPosition(JLabel.CENTER);
+        button2.setVerticalTextPosition(JLabel.BOTTOM);
+        button2.setFocusable(false);
+        button2.setIcon(icon);
+        button2.setEnabled(false);
+        button2.setIconTextGap(-15);
+        button2.setForeground(new Color(0xAA00FF));
+        button2.setBorder(BorderFactory.createEtchedBorder());
+        button2.addActionListener(this);
+
+        label = new JLabel();
+        label.setText(String.valueOf(i));
+        label.setBounds(220, 60, 150, 150);
+        label.setFont(new Font("Consolas", Font.BOLD, 96));
+        button1.setFont(new Font("Consolas", Font.BOLD, 22));
+        button2.setFont(new Font("Consolas", Font.BOLD, 22));
+        label.setVerticalTextPosition(JLabel.CENTER);
+        label.setForeground(new Color(0x9A00FF));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(640, 600);
+        this.setVisible(true);
+        this.add(button1);
+        this.add(button2);
+        this.add(label);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button1){
+            i++;
+            System.out.printf("You clicked the button %d times\n", i);
+            label.setText(String.valueOf(i));
+            button1.setEnabled(false); // makes the button disabled once clicked
+            button2.setEnabled(true);
+        }
+        else if(e.getSource() == button2){
+            i++;
+            System.out.printf("You clicked the button %d times\n", i);
+            label.setText(String.valueOf(i));
+            button1.setEnabled(true); // makes the button disabled once clicked
+            button2.setEnabled(false);
+        }
+    }
+
 }
