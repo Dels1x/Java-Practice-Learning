e85beacab717ab3a7e1c698c907ed3d8a5c192ef
Dels1x <delsixreborn@gmail.com>
2022-06-09 14:09:57 +0300

    56. LayeredPane

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,38 @@ public class Main {
 
         // Layout Manager = Defines natural layout for components within a container
 
-        // GridLayout = places components in a grid of cells
-        //              Each component takes all the available space within its cell,
-        //              and each cell is the same size.
+        // JLayeredPane = Swing container that provides a third dimension for positioning components. ex. depth, Z-index
 
-        JFrame frame = new JFrame();
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 500, 500);
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.red);
+        label1.setBounds(50, 50, 200, 200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.green);
+        label2.setBounds(100, 100, 200, 200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.blue);
+        label3.setBounds(150, 150, 200, 200);
+
+        JFrame frame = new JFrame("JLayeredPane");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(3, 3, 5, 8));
-        frame.setSize(600, 600);
+        frame.setSize(new Dimension(500, 500));
+        frame.setLayout(null);
 
-        for(int i = 1; i < 10; i++){
-            frame.add(new JButton(String.valueOf(i)));
-        }
+
+        layeredPane.add(label1, Integer.valueOf(2));
+        layeredPane.add(label2, Integer.valueOf(0));
+        layeredPane.add(label3, Integer.valueOf(1)); // the higher the number, the higher component is
+
+        frame.add(layeredPane);
+
 
         frame.setVisible(true);
 
