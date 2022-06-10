69148c020616a89f9b6a220debcbf76632824fd2
Dels1x <delsixreborn@gmail.com>
2022-06-10 22:36:09 +0300

    59. textfield

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // Layout Manager = Defines natural layout for components within a container
+        // JTextField = A GUI textbox component that can be used to add, set, or get text
 
-        // JLayeredPane = Swing container that provides a third dimension for positioning components. ex. depth, Z-index
-
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setBounds(0, 0, 500, 500);
-
-
-        JFrame frame = new JFrame("JLayeredPane");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(500, 500));
-        frame.setLayout(null);
-
-
-        frame.setVisible(true);
-
+        MyFrame frame = new MyFrame();
 
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,71 +4,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
-    JButton button1;
-    JButton button2;
-    JLabel label;
-    int i = 0;
-    MyFrame(){
 
-        ImageIcon icon = new ImageIcon("diamond_helmet_64x64.png");
-        button1 = new JButton();
-        button2 = new JButton();
-        button1.setBounds(50, 50, 150, 150);
-        button1.setText("Click me");
-        button1.setHorizontalTextPosition(JLabel.CENTER);
-        button1.setVerticalTextPosition(JLabel.BOTTOM);
-        button1.setFocusable(false);
-        button1.setIcon(icon);
-        button1.setIconTextGap(-15);
-        button1.setForeground(new Color(0xAA00FF));
-        button1.setBorder(BorderFactory.createEtchedBorder());
-        button1.addActionListener(this);
+    JTextField textbox;
+    JButton submitButton;
 
-        button2 = new JButton();
-        button2.setBounds(50, 200, 150, 150);
-        button2.setText("Click me");
-        button2.setHorizontalTextPosition(JLabel.CENTER);
-        button2.setVerticalTextPosition(JLabel.BOTTOM);
-        button2.setFocusable(false);
-        button2.setIcon(icon);
-        button2.setEnabled(false);
-        button2.setIconTextGap(-15);
-        button2.setForeground(new Color(0xAA00FF));
-        button2.setBorder(BorderFactory.createEtchedBorder());
-        button2.addActionListener(this);
-
-        label = new JLabel();
-        label.setText(String.valueOf(i));
-        label.setBounds(220, 60, 150, 150);
-        label.setFont(new Font("Consolas", Font.BOLD, 96));
-        button1.setFont(new Font("Consolas", Font.BOLD, 22));
-        button2.setFont(new Font("Consolas", Font.BOLD, 22));
-        label.setVerticalTextPosition(JLabel.CENTER);
-        label.setForeground(new Color(0x9A00FF));
+    MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(640, 600);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.setSize(new Dimension(720, 720));
+
+        textbox = new JTextField(); // create a textbox
+        submitButton = new JButton("Submit");
+        submitButton.addActionListener(this);
+
+        textbox.setPreferredSize(new Dimension(250, 50)); // set size
+        textbox.setFont(new Font("Consolas", Font.PLAIN, 36)); // set font
+        textbox.setForeground(new Color(0xFF561F)); // set font color
+        textbox.setBackground(Color.darkGray); // set background color
+        textbox.setCaretColor(Color.white); // set this line when you type something color
+        textbox.setText("delsix"); // set default text
+
+
+        this.add(textbox);
+        this.add(submitButton);
+        this.pack();
         this.setVisible(true);
-        this.add(button1);
-        this.add(button2);
-        this.add(label);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == button1){
-            i++;
-            System.out.printf("You clicked the button %d times\n", i);
-            label.setText(String.valueOf(i));
-            button1.setEnabled(false); // makes the button disabled once clicked
-            button2.setEnabled(true);
-        }
-        else if(e.getSource() == button2){
-            i++;
-            System.out.printf("You clicked the button %d times\n", i);
-            label.setText(String.valueOf(i));
-            button1.setEnabled(true); // makes the button disabled once clicked
-            button2.setEnabled(false);
+        if(e.getSource() == submitButton) {
+            textbox.setEditable(false); // make textbox uneditable
+            submitButton.setEnabled(false); // makes button disabled
+            System.out.println("Welcome "+textbox.getText());
         }
     }
 
