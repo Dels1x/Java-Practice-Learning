cfa9bea40efc95149daf612d5e5d89920dfa700b
Dels1x <delsixreborn@gmail.com>
2022-06-13 21:50:33 +0300

    Centered panel

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String [] args) {
 
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(100, 100));
-        panel.setBackground(Color.RED);
-
-        frame.setLayout(new GridBagLayout());
-        frame.add(panel, new GridBagConstraints());
-        frame.setSize(new Dimension(800, 800));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        ProgressBarDemo progressBarDemo = new ProgressBarDemo();
 
     }
 }
--- src/ProgressBarDemo.java
+++ src/ProgressBarDemo.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class ProgressBarDemo {
 
     JFrame frame = new JFrame();
-    JProgressBar bar = new JProgressBar();
+    JProgressBar bar = new JProgressBar(0, 100);
 
     int frameWidth = 626;
 
@@ -15,16 +15,36 @@ public class ProgressBarDemo {
         frame.setVisible(true);
 
         bar.setValue(0);
-        bar.setBounds(0, 0, frame.getContentPane().getWidth() - 50, 61);
+        bar.setBounds(0, 0, frame.getContentPane().getWidth(), frame.getContentPane().getWidth() / 8);
         bar.setStringPainted(true);
         bar.setBackground(new Color(0x1C2244));
+        bar.setForeground(new Color(0x00AAFF));
+        bar.setFont(new Font("Arial", Font.BOLD, 50));
 
 
         frame.add(bar);
 
-        System.out.println(frame.getWidth());
-        System.out.println(frame.getHeight());
-        System.out.println(frame.getContentPane().getWidth());
-        System.out.println(frame.getContentPane().getHeight());
+        System.out.println(frame.getWidth()); // gets you width of frame
+        System.out.println(frame.getHeight()); // gets you height of frame
+        System.out.println(frame.getContentPane().getWidth()); // gets you width of content pane
+        System.out.println(frame.getContentPane().getHeight()); // gets you height of content pane
+
+        fill();
+    }
+
+    public void fill() {
+        int counter = 0;
+
+        while(counter <= 100) {
+            counter++;
+            bar.setValue(counter);
+            try {
+                Thread.sleep(32);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        bar.setString("Done! :)");
     }
 }
