8e6ca53c0bdba815db2da007495dcf0b456d53f7
Dels1x <delsixreborn@gmail.com>
2022-06-14 11:55:53 +0300

    GridLayout repeat

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ import javax.swing.*;
 public class Main {
     public static void main(String [] args) {
 
-        ProgressBarDemo progressBarDemo = new ProgressBarDemo();
+        JFrame frame = new JFrame();
+
+        frame.setSize(new Dimension(600, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(3, 3, 5, 5));
+
+        for(int i = 0; i < 9; i++) {
+            frame.add(new JButton(String.valueOf(i+1)));
+        }
+
+
+        frame.setVisible(true);
 
     }
 }
