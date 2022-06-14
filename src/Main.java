6e731e0aa1f6a613f578fe4e9f2e7671366bab74
Dels1x <delsixreborn@gmail.com>
2022-06-14 13:07:59 +0300

    65. menu bar

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String [] args) {
 
-        JFrame frame = new JFrame();
-
-        frame.setSize(new Dimension(600, 600));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(3, 3, 5, 5));
-
-        for(int i = 0; i < 9; i++) {
-            frame.add(new JButton(String.valueOf(i+1)));
-        }
-
-
-        frame.setVisible(true);
+        new MyFrame();
 
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,42 +2,105 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    JComboBox<String> comboBox;
-    String[] genders;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenu editMenu;
+    JMenu helpMenu;
+
+    JMenuItem saveItem;
+    JMenuItem loadItem;
+    JMenuItem exitItem;
+    JMenuItem saveAsItem;
+
+    ImageIcon exitIcon = new ImageIcon("exit-16.png");
+    ImageIcon saveIcon = new ImageIcon("save-16.png");
+    ImageIcon saveAsIcon = new ImageIcon("save-as-16.png");
+    ImageIcon loadIcon = new ImageIcon("load-16.png");
+
 
     MyFrame() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(600, 600);
         this.setLayout(new FlowLayout());
-        this.setSize(new Dimension(800, 600));
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        genders = new String[]{"male", "female", "other"};
-        comboBox = new JComboBox<>(genders); // put only Wrapper datatypes arrays here (Integer[] instead of int[])
-        comboBox.addActionListener(this);
-        comboBox.setEditable(true); // set editable (bad idea)
-        System.out.println(comboBox.getItemCount()); // get amount of items
-        comboBox.addItem("genderfluid helisexual"); // add item to the end of the list
-        comboBox.insertItemAt("genius", 0); // insert item at index 0
-        comboBox.setSelectedIndex(0); // selected item by default
-        comboBox.removeItemAt(2); // delete women
-        comboBox.removeAllItems(); // removes all items from the combobox
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        helpMenu = new JMenu("Help");
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
 
-        this.add(comboBox);
-        this.pack();
+        loadItem = new JMenuItem("Load");
+        saveItem = new JMenuItem("Save");
+        saveAsItem  = new JMenuItem("Save as...");
+        exitItem = new JMenuItem("Exit");
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+        fileMenu.add(exitItem);
+
+        exitItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        loadItem.addActionListener(this);
+        saveAsItem.addActionListener(this);
+
+        fileMenu.setMnemonic(KeyEvent.VK_F); // press alt + F for File
+        editMenu.setMnemonic(KeyEvent.VK_E); // press alt + E for Exit
+        helpMenu.setMnemonic(KeyEvent.VK_H); // press alt + H for Help
+
+        loadItem.setMnemonic(KeyEvent.VK_L); // press L to load
+        saveItem.setMnemonic(KeyEvent.VK_S); // press S to save
+        saveAsItem.setMnemonic(KeyEvent.VK_A); // press A to save as
+        exitItem.setMnemonic(KeyEvent.VK_E); // press E to exit
+
+        exitItem.setIcon(exitIcon); // sets Icon for exitItem
+        saveItem.setIcon(saveIcon); // sets Icon for saveItem
+        saveAsItem.setIcon(saveAsIcon); // sets Icon for saveAsItem
+        loadItem.setIcon(loadIcon); // sets Icon for loadItem
+
+
+
+        this.setJMenuBar(menuBar);
         this.setVisible(true);
-
-        // i'm noob
-
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == comboBox) {
-            System.out.println("You are "+comboBox.getSelectedItem());
-            System.out.println("You are "+genders[comboBox.getSelectedIndex()]);
+        if(e.getSource() == exitItem) {
+            System.exit(666);
+        }
+        else if(e.getSource() == saveItem) {
+            System.out.println("We saved the file");
+        }
+        else if(e.getSource() == saveItem) {
+            System.out.println("We saved the file in a way you want :D");
+        }
+        else if(e.getSource() == loadItem) {
+            System.out.println("Loading file.");
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            System.out.println("Loading file..");
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            System.out.println("Loading file...");
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            System.out.println("File loaded");
         }
     }
 }
