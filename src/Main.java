b68979f6e8f0b820b475833d5bebbadd374d5098
Dels1x <delsixreborn@gmail.com>
2022-06-21 22:32:13 +0300

    6kyu: Split Strings

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,49 @@
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args) {
+        String s1 = "LovePizza";
+        String[] array = solution(s1);
 
-        // bind an Action to a KeyStroke
-        // don't require you to click a component to give it focus
-        // all Swing components have ability to use KeyBindings
-        // increased flexibility (гибкость) compared to KeyListeners
-        // can assign key strokes to invidual Swing components
-        // more difficult to utilize and set up
+        for(String i : array) {
+            System.out.println(i);
+        }
 
-        new Game();
 
     }
+
+    public static String[] solution(String s) {
+        int arrayLength;
+        if(s.length() % 2 == 0){
+            arrayLength = s.length() / 2;
+        }
+        else{
+            arrayLength = 1 + s.length() / 2;
+        }
+        String[] lettersArray = new String[arrayLength];
+        String str = "";
+        System.out.println("array length: " +arrayLength);
+        for(int i = 0; i < s.length(); i+=2){
+            if(s.length() % 2 == 0) {
+                str = s.substring(i, i+2);
+            }
+            else {
+                if(i+1 == s.length()){
+                    str = s.substring(i) + "_";
+                    System.out.println(s.substring(i) + "_");
+                }
+                else{
+                    str = s.substring(i, i+2);
+                    System.out.println(s.substring(i, i+2));
+                }
+            }
+            lettersArray[i / 2] = str;
+            System.out.println(lettersArray);
+
+        }
+        
+
+        return lettersArray;
+    }
+
 }
