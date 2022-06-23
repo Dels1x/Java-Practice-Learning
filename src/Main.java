c2a4e131fd942bde316b1ea74cb56e52f4a419a8
Dels1x <delsixreborn@gmail.com>
2022-06-23 21:28:52 +0300

    80. lambda

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,16 @@
-import java.util.TreeMap;
-
 public class Main {
     public static void main(String [] args) {
 
-        System.out.println(s(5));
+        String str = "s1mple G.O.A.T.";
+        char lol = '1';
+
+        MyInteface myInterface = (x, y) -> System.out.println(x);
+        myInterface.message(str, lol);
+    }
+
+    public static int solution(int number) {
+        return number * 2;
 
     }
 
-    public static String solution(int n) {
-        TreeMap<Integer, String> romanNumerals = new TreeMap<>();
-
-        if(n > 0) {
-
-            romanNumerals.put(1, "I");
-            romanNumerals.put(4, "IV");
-            romanNumerals.put(5, "V");
-            romanNumerals.put(9, "IX");
-            romanNumerals.put(10, "X");
-            romanNumerals.put(40, "XL");
-            romanNumerals.put(50, "L");
-            romanNumerals.put(90, "XC");
-            romanNumerals.put(100, "C");
-            romanNumerals.put(400, "CD");
-            romanNumerals.put(500, "D");
-            romanNumerals.put(900, "CM");
-            romanNumerals.put(1000, "M");
-
-            int romanFloor =  romanNumerals.floorKey(n); // floorkey returns the closest lower value possible to get than n
-            if ( n == romanFloor ) {
-                return romanNumerals.get(n);
-            }
-            return romanNumerals.get(romanFloor) + solution(n-romanFloor); // recursion
-        }
-
-        return "";
-
-    }
-
-    public static String s(int num)  {
-        if(num > 0) {
-            return "s" + s(num - 1);
-        }
-        else {
-            return "";
-        }
-    }
 }
-
-// that was so hard
---
+++ src/MyInteface.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface MyInteface {
+    public void message(String str, char symbol);
+}
