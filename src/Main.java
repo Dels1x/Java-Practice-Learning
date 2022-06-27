fead8c23041559844ec141f2a5741e00775946b9
Dels1x <delsixreborn@gmail.com>
2022-06-27 13:14:54 +0300

    get min and max value in an array

--- src/Main.java
+++ src/Main.java
@@ -7,58 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(high("a b c d e f"));
-    }
-
-    public static String high(String s) {
-        Character currentChar;
-        Character alphabetChar;
-        String word;
-
-        String[] words = s.split(" ");
-        int[] scores = new int[words.length];
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        int[] numbers = {1,2,3,4,5};
 
 
-        for(int i = 0; i < words.length; i++) {
-            word = words[i];
-            System.out.println(word);
+        System.out.println(Arrays.stream(numbers).min().getAsInt());
+        System.out.println(Arrays.stream(numbers).max().getAsInt());
 
-            for(int j = 0; j < word.length(); j++) {
-                currentChar = word.charAt(j);
-
-                System.out.println(currentChar);
-
-                for(int a = 0; a < alphabet.length; a++) {
-                    alphabetChar = alphabet[a];
-
-                    if(currentChar.equals(alphabetChar)) {
-                        scores[i] += a+1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        // for(int score : scores) System.out.println(score);
-        // for(String word1 : words) System.out.println(word1);
-
-        return words[getIndexOfLargest(scores)];
-
-    }
-
-    public static int getIndexOfLargest(int[] array) {
-        int largest = 0;
-        int index = 0;
-
-        for(int i = 0; i < array.length; i++) {
-            if(array[i] > largest) {
-                largest = array[i];
-                index = i;
-            }
-        }
-
-        return index;
     }
 
 }
