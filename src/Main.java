e1a6edc8c38e127cdca9607cd62eb8de6049c022
Dels1x <delsixreborn@gmail.com>
2022-07-04 14:53:02 +0300

    13.2 Multithreading in Java Practical

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-import java.math.*;
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.IntStream;
-import java.time.*;
+class Hi extends Thread {
+    public void run() {
+        for(int i = 5; i > 0; i--) {
+            System.out.println("Hi");
+            try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        }
+    }
+}
 
+class Bye extends Thread {
+    public void run() {
+        for(int i = 5; i > 0; i--) {
+            System.out.println("Bye");
+            try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        }
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Hi hi = new Hi();
+        Bye bye = new Bye();
 
-        Thread myThread = new MyThread();
-        myThread.run();
+        hi.start();
+        try {Thread.sleep(250);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        bye.start();
 
-        for(int i = 50; i >= 0; i--) {
-            System.out.println(i);
-        }
     }
-
-
-
 }
