a5f20de3852974dfc62d39949ddda6451306529a
Dels1x <delsixreborn@gmail.com>
2022-07-04 15:59:45 +0300

    13.4 MultiThreading using Lambda Expression

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,19 @@
-class FatherClass {
-    String hello = "Hello from Miami, dude!";
-    String hi = "Hi man! We are niggas with attitudes! Straight out of compton!";
-}
-
-class Hi extends FatherClass implements Runnable {
-    public void run() {
-        for(int i = 5; i > 0; i--) {
-            System.out.println(hi);
-            try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
-        }
-    }
-}
-
-class Bye extends FatherClass implements Runnable {
-    public void run() {
-        for(int i = 5; i > 0; i--) {
-            System.out.println(hello);
-            try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
-        }
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Runnable hi = new Hi();
-        Runnable bye = new Bye();
+        Runnable hi = () -> {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Hello from Miami, dude!");
+                try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
+            }
+        };
+        Runnable bye = () -> {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Hi man! We are niggas with attitudes! Straight out of compton!");
+                try {Thread.sleep(500);} catch (InterruptedException e) {throw new RuntimeException(e);}
+            }
+        };
 
         Thread thread1 = new Thread(hi);
         Thread thread2 = new Thread(bye);
