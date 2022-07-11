54081fca9973ab7e2f7a4f82671566ab4570f228
Dels1x <delsixreborn@gmail.com>
2022-07-11 18:14:05 +0300

    Detect Pangram
    https://www.codewars.com/kata/545cedaa9943f7fe7b00004800011

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,27 @@
+import java.awt.print.Pageable;
 import java.util.Arrays;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(M2048.merge(new int[] {8, 8, 2, 4})));
+        System.out.println(PangramChecker.check("The quick brown fox jumps over the lazy dog."));
 
     }
 
 
 }
 
+class PangramChecker {
+    public static boolean check(String sentence){
+        String[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("");
 
-class M2048 {
-
-    static int[] merge(int[] line) {
-        int temp;
-        line = moveZerosToEnd(line);
-        for(int i = 0; i < line.length-1; i++) {
-            temp = line[i];
-            if(line[i+1] == temp) {
-                line[i] = temp*2;
-                line[i+1] = 0;
-            }
-        }
-        return moveZerosToEnd(line);
-    }
-
-    static int[] moveZerosToEnd(int[] line) {
-        int count = 0;
-
-        for(int i = 0; i < line.length; i++) {
-            if(line[i] != 0) {
-                line[count++] = line[i];
-            }
+        for(int i = 0; i < alphabetArray.length; i++) {
+            if(!sentence.toLowerCase().contains(alphabetArray[i])) return false;
         }
 
-        while(count < line.length) {
-            line[count++] = 0;
-        }
-
-        return line;
+        return true;
     }
 }
+
 
