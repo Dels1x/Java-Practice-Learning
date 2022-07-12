8647299494b2ac64862f21eda733a4619636e86d
Dels1x <delsixreborn@gmail.com>
2022-07-12 18:42:25 +0300

    Counting Duplicates
    https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,34 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
-        PaginationHelper ph = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'v', 'd', 'l', 'a', 'b', 'c', 'v', 'd', 'l', 'a', 'b', 'c', 'v', 'd', 'l', 'a', 'b', 'c', 'v', 'd', 'l'), 10);
-        System.out.println(ph.pageIndex(0));
+        CountingDuplicates e = new CountingDuplicates();
+
+        System.out.println(CountingDuplicates.duplicateCount("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzReturnsTwentySix"));
 
     }
 
 
 }
 
+class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        int count = 0;
+        String temp;
+        text = text.toLowerCase();
+
+        while(text != "") {
+            temp = text.substring(0, 1);
+            text = text.substring(1);
+            if (text.contains(temp)) {
+                count++;
+                text = text.replace(temp, "");
+            }
+        }
+
+        return count;
+    }
+}
