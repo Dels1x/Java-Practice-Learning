38ff4cb1f03199be8ee17e32e9e96dc91486aacb
Danislav <delsixreborn@gmail.com>
2022-07-15 14:24:23 +0300

    Bit Counting
    https://www.codewars.com/kata/526571aae218b8ee490006f4/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.println(Kata.ascendDescend(25, -9, -3));
+        System.out.println(Kata.countBits(1234));
 
     }
 }
 
 class Kata {
+    public static int countBits(int n){
+        String binaryNum = Integer.toBinaryString(n);
+        int output = 0;
 
-    public static String ascendDescend(int length, int minimum, int maximum) {
-        if (minimum > maximum || length == 0) return "";
-        String output = "";
-        int num = minimum;
-        boolean ascending = true;
-
-        while (output.length() < length) {
-            if (num == maximum) ascending = false;
-            else if (num == minimum) ascending = true;
-
-            if (ascending)
-                output = output.concat(String.valueOf(num++));
-            else if (minimum != maximum)
-                output = output.concat(String.valueOf(num--));
-            else {
-                output = output.concat(String.valueOf(num));
-            }
-
-            System.out.println(output);
-            System.out.println(output.length());
+        for(int i = 0; i < binaryNum.length(); i++) {
+            output += Character.getNumericValue(binaryNum.charAt(i));
         }
-        return output.substring(0, length);
+
+        return output;
     }
 }
