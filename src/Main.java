1887f33465a231f0a75759e8d7fc28465c939d9e
Danislav <delsixreborn@gmail.com>
2022-07-16 13:50:50 +0300

    Data Structures and Algorithms: 3. Queue

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,47 @@
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(Kata.dirReduc(new String[]{"NORTH", "NORTH", "SOUTH", "SOUTH", "WEST", "NORTH", "EAST"})));
+        /*
+                Queue = FIFO data structure. First-In First-Out
+                        A collection designed for holding elements prior to processing
+                        Linear data structure
 
-    }
-}
+                        add = enqueue, offer()
+                        remove = dequeue, poll()
+        */
 
-class Kata {
-    public static String[] dirReduc(String[] arr) {
-        final Stack<String> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>();
 
-        for (final String direction : arr) {
-            final String lastElement = !stack.isEmpty() ? stack.lastElement() : null;
+        queue.offer("Danya Kostyanitca");
+        queue.offer("Danya Hamula");
+        queue.offer("Danislav Anufriev");
+        queue.offer("Kirill Charyshnikov");
 
-            switch (direction) {
-                case "NORTH" -> {if("SOUTH".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
-                case "SOUTH" -> {if("NORTH".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
-                case "EAST" -> {if("WEST".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
-                case "WEST" -> {if("EAST".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
-            }
-        }
 
-        return stack.toArray(String[]::new);
+        System.out.println(queue);
+        System.out.println(queue.peek()); // returns first element
+        System.out.println(queue.poll()); // removes first element and returns it
+        System.out.println(queue.size()); // returns size of queue
+        System.out.println(queue.contains("Danya Hamula")); // checks if queue contains the string
+
+        queue.poll();
+        queue.poll();
+        queue.poll();
+        queue.poll();
+        // queue.element() - throws exception
+        queue.poll(); // - doesn't throw
+
+        System.out.println(queue);
+        System.out.println(queue.isEmpty());
+
+        // Where are queues useful?
+
+        // 1. Keyboard Buffer (letters should appear on the screen in the order they're pressed)
+        // 2. Printer Queue (Print jobs should be completed in order)
+        // 3. Used in LinkedLists, PriorityQueues, Breadth-first-search
+
     }
 }
