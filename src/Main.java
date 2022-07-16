03636b6c96821df247b531a096984464fe72d49f
Danislav <delsixreborn@gmail.com>
2022-07-16 12:18:23 +0300

    Directions Reduction
    https://www.codewars.com/kata/550f22f4d758534c1100025a/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,30 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Kata.validParentheses(")(())"));
+        System.out.println(Arrays.toString(Kata.dirReduc(new String[]{"NORTH", "NORTH", "SOUTH", "SOUTH", "WEST", "NORTH", "EAST"})));
 
     }
 }
 
 class Kata {
-    public static boolean validParentheses(String parens) {
-        int count = 0;
+    public static String[] dirReduc(String[] arr) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
 
-        for(int i = 0; i < parens.length(); i++) {
-            if(parens.charAt(i) == '(') count++;
-            else if(parens.charAt(i) == ')') count--;
-
-            if(count < 0) return false;
+        for(int i = 1; i < list.size(); i++) {
+            if((list.get(i).equals("NORTH") && list.get(i-1).equals("SOUTH")) ||
+               (list.get(i).equals("SOUTH") && list.get(i-1).equals("NORTH")) ||
+               (list.get(i).equals("EAST") && list.get(i-1).equals("WEST")) ||
+               (list.get(i).equals("WEST") && list.get(i-1).equals("EAST"))) {
+                System.out.println("tf");
+                    list.remove(i);
+                    list.remove(i-1);
+                    i = 0;
+            }
         }
 
-        return count == 0;
+        return list.toArray(new String[0]);
     }
 }
