b8a1cc161b25354f372283dc444cf187a4f6d32e
Danislav <delsixreborn@gmail.com>
2022-07-16 13:20:32 +0300

    Directions Reduction kata using stacks

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,29 @@
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        /*
-            stack = a LIFO data structure. Last-in First-out
-            stores objects into a sort of "vertical tower"
-            push() to add to the top
-            pop() to remove from the top
-        */
+        System.out.println(Arrays.toString(Kata.dirReduc(new String[]{"NORTH", "NORTH", "SOUTH", "SOUTH", "WEST", "NORTH", "EAST"})));
 
-        Stack<String> stack = new Stack<>();
+    }
+}
 
-        System.out.println(stack.empty()); // returns true or false whether if the stack is empty
+class Kata {
+    public static String[] dirReduc(String[] arr) {
+        final Stack<String> stack = new Stack<>();
 
-        stack.push("Minecraft");
-        stack.push("The Neverhood");
-        stack.push("Mafia: the City of Lost Heaven");
-        stack.push("Detroit: Become Human");
-        stack.push("The Witcher");
-        stack.push("The Witcher II");
+        for (final String direction : arr) {
+            final String lastElement = !stack.isEmpty() ? stack.lastElement() : null;
 
-        System.out.println(stack);
+            switch (direction) {
+                case "NORTH" -> {if("SOUTH".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
+                case "SOUTH" -> {if("NORTH".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
+                case "EAST" -> {if("WEST".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
+                case "WEST" -> {if("EAST".equals(lastElement)){stack.pop();} else {stack.push(direction);}}
+            }
+        }
 
-        stack.pop();
-        stack.pop();
-        stack.pop();
-
-        String myFavGame = stack.pop();
-
-        System.out.println(myFavGame);
-
-        System.out.println(stack.search("The Witcher")); // return -1 because The Witcher is not anymore in the stack since we popped it
-        System.out.println(stack.search("Minecraft")); // return index from top
-
-        /*  uses of stacks?
-            1. undo/redo features in text editors
-            2. moving back/forward through the browser history
-            3. backtracking algorithms (maze, file directories)
-            4. calling functions (call stack)
-        */
+        return stack.toArray(String[]::new);
     }
 }
