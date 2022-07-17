e0e385e76a55b5a4575dfba6d2641768be299880
Danislav <delsixreborn@gmail.com>
2022-07-17 12:16:33 +0300

    CodeWars: Tribonacci Sequence
    https://www.codewars.com/kata/556deca17c58da83c00002db/train/java

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(Kata.tribonacci(new double[]{13, 10, 20}, 2)));
+        System.out.println(Arrays.toString(Kata.tribonacci(new double[]{13, 10, 20}, 8)));
 
     }
 }
 
 class Kata {
     public static double[] tribonacci(double[] s, int n) {
-        ArrayList<Double> arrayList = new ArrayList<>();
+        double[] array = Arrays.copyOf(s, n);
 
-        if (n > 2) {
-            for (double i : s) arrayList.add(i);
-        } else {
-            for (int i = 0; i < n; i++) arrayList.add(s[i]);
-            return arrayList.stream().mapToDouble(d ->d).toArray();
-        }
+        for(int i = 3; i < n; i++) array[i] = array[i-3]+array[i-2]+array[i-1];
 
-        for(int i = 2; i < n-1; i++) arrayList.add(arrayList.get(i - 2) + arrayList.get(i - 1) + arrayList.get(i));
-
-        return arrayList.stream().mapToDouble(d -> d).toArray();
+        return array;
     }
 }
