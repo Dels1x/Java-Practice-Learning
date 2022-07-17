da3eabc32074ad402e93fc61926df5ffbe8f11a0
Danislav <delsixreborn@gmail.com>
2022-07-17 12:46:29 +0300

    Data Structures and Algorithms: 5. Linked Lists

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,47 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(Kata.tribonacci(new double[]{13, 10, 20}, 8)));
+        LinkedList<String> linkedList = new LinkedList<>();
 
-    }
-}
+        /*
+        linkedList.push("A");
+        linkedList.push("B");
+        linkedList.push("C");
+        linkedList.push("D");
+        linkedList.push("F");
+        linkedList.push("S");
+        linkedList.pop(); */
 
-class Kata {
-    public static double[] tribonacci(double[] s, int n) {
-        double[] array = Arrays.copyOf(s, n);
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        linkedList.offer("S");
+        linkedList.poll();
 
-        for(int i = 3; i < n; i++) array[i] = array[i-3]+array[i-2]+array[i-1];
+        linkedList.add(4, "E");
 
-        return array;
+        System.out.println(linkedList);
+
+        linkedList.remove("E");
+
+        System.out.println(linkedList);
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+
+        linkedList.addFirst("0");
+        linkedList.addLast("9");
+        String first = linkedList.removeFirst();
+        String last = linkedList.removeLast();
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.get(3));
+        System.out.println(linkedList.size());
+
     }
 }
