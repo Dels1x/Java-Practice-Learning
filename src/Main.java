82e2df5f787727e4a7c07c1ff43f2e59b4a852ab
Danislav <delsixreborn@gmail.com>
2022-07-18 21:31:04 +0300

    Data Structures and Algorithms: #7  LinkedLists vs ArrayLists ️

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,50 @@
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1,2,3,4,3,2,1};
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[] array = new int[1000000];
 
-        System.out.println(Kata.findEvenIndex(array));
-    }
-}
+        long startTime;
+        long endTime;
+        long elapsedTime;
 
-
-class Kata {
-    public static int findEvenIndex(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if(Arrays.stream(Arrays.copyOfRange(arr, 0, i)).sum() ==
-               Arrays.stream(Arrays.copyOfRange(arr, i+1, arr.length)).sum()) return i;
+        for(int i = 0; i < 1000000; i++) {
+            linkedList.add(i);
+            arrayList.add(i);
+            array[i] = i;
         }
 
-        return -1;
+        startTime = System.nanoTime();
+
+        //linkedList.add(5, -5);
+        //linkedList.get(500000);
+        //linkedList.remove(500000);
+        linkedList.remove(999999);
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+
+        System.out.println("LinkedList:\t"+elapsedTime);
+
+        startTime = System.nanoTime();
+
+        //arrayList.add(5, -5);
+        //arrayList.get(500000);
+        //arrayList.remove(500000);
+        arrayList.remove(999999);
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+
+        System.out.println("ArrayList:\t"+elapsedTime);
+
     }
 }
+
+
